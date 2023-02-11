ec2e31b4b17750221c88ae1881c1c58dc0cf1c61
bekatadesse <bekatadesse5@gmail.com>
2023-02-11 14:56:25 +0000

    task1

--- 0x00-MySQL_Advanced/1-country_users.sql
+++ 0x00-MySQL_Advanced/1-country_users.sql
@@ -1,5 +1,5 @@
 -- create table user
-CREATE TABLE IF NOT EXISTS `users` ('id' int NOT NULL PRIMARY KEY AUTO_INCREMENT,
-    'email' varchar(255) NOT NULL UNIQUE,
-    'name' varchar(255) NOT NULL,
-    'country'  ENUM('US', 'CO', 'TN') NOT NULL DEFAULT 'US');
+CREATE TABLE IF NOT EXISTS `users` (id int NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    email varchar(255) NOT NULL UNIQUE,
+    name varchar(255) NOT NULL,
+    country  ENUM('US', 'CO', 'TN') NOT NULL DEFAULT 'US');
