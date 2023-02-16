b0869f408e0e1546d7ee7fcb568d87e1c90a4637
bekatadesse <bekatadesse5@gmail.com>
2023-02-16 02:53:32 +0000

    task 4

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -8,6 +8,32 @@ from typing import Union, Optional, Callable
 import uuid
 from functools import wraps
 
+
+class CacheError(Exception):
+    '''Base class for exceptions in this module.'''
+    pass
+
+def replay(method: Callable) -> None:
+    """
+    display the history of calls of a particular function
+    """
+    key = method.__qualname__
+    key_inputs = key + ":inputs"
+    key_outputs = key + ":outputs"
+
+    redis = method.__self__._redis
+
+    count = redis.get(key).decode("utf-8")
+    print("{} was called {} times:".format(key, count))
+
+    inputList = redis.lrange(key_inputs, 0, -1)
+    outputList = redis.lrange(key_outputs, 0, -1)
+
+    all_data = list(zip(inputList, outputList))
+    for i, j in all_data:
+        attr, data = i.decode("utf-8"), j.decode("utf-8")
+        print("{}(*{}) -> {}".format(key, attr, data))
+
 def call_history(method: Callable) -> Callable:
     """
     store the history of inputs and
