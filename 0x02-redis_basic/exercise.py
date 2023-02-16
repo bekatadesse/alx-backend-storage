8645df780cbf7430e0beceb7f285a0749d60d901
bekatadesse <bekatadesse5@gmail.com>
2023-02-16 02:39:27 +0000

    task 2

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -32,3 +32,27 @@ class Cache:
         self._redis.set(key, data)
 
         return key
+
+    def get(self,
+            key: str,
+            fn: Optional[Callable] = None
+            ) -> Union[str, bytes, int, float]:
+        """
+        Read from redis and
+        recover original type
+        """
+        data = self._redis.get(key)
+
+        return fn(data) if fn else data
+
+    def get_str(self, data: bytes) -> str:
+        """
+        convert bytes to string
+        """
+        return data.decode('utf-8')
+
+    def get_int(self, data: bytes) -> int:
+        """
+        convert string to integer
+        """
+        return int(data)
